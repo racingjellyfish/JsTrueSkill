9a18a8389ef57bcf0054fad226d5f8173e46fd83
Adrian Conlin <adrian.conlin@gmail.com>
2013-07-24 11:15:23 +0100

    Implemented errorFunctionCumulativeTo, cumulativeTo and at calculations.

--- src/racingjellyfish/jstrueskill/numerics/GaussianDistribution.js
+++ src/racingjellyfish/jstrueskill/numerics/GaussianDistribution.js
@@ -1,5 +1,8 @@
 var MathUtils = require('./MathUtils');
 var square = MathUtils.square;
+
+var INV_SQRT_2 = -0.7071067811865476;
+
 /**
  * Immutable representation of the Gaussian distribution of one variable. Not normalized:
  *
@@ -115,6 +118,86 @@ GaussianDistribution.fromPrecisionMean = function(precisionMean, precision) {
 		1.0 / precision, precision, precisionMean);
 };
 
+/**
+ * Return the cumulative area to the specified value under the specified gaussian.
+ *
+ * N.B. If mean and/or standard deviation are not specified the
+ * normalized values will be used.
+ *
+ */
+GaussianDistribution.cumulativeTo = function(x, mean, standardDeviation) {
+	// TODO is std dev required?
+	// Check arguments
+	if (!mean) {
+		mean = 0;
+	}
+	if (!standardDeviation) {
+		standardDeviation = 1;
+	}
+
+	var result = GaussianDistribution.errorFunctionCumulativeTo(INV_SQRT_2 * x);
+	return 0.5 * result;
+};
+
+/**
+ * Return the cumulative error function to the specifed value.
+ *
+ * Derived from page 265 of Numerical Recipes 3rd Edition
+ */
+GaussianDistribution.errorFunctionCumulativeTo = function(x) {
+	var z = Math.abs(x);
+	var t = 2.0 / (2.0 + z);
+	var ty = 4 * t - 2;
+	var coefficients = [
+		-1.3026537197817094, 0.6419697923564902, 0.019476473204185836,
+		-0.00956151478680863, -9.46595344482036E-4, 3.66839497852761E-4,
+		4.2523324806907E-5, -2.0278578112534E-5, -1.624290004647E-6,
+		1.30365583558E-6, 1.5626441722E-8, -8.5238095915E-8, 6.529054439E-9,
+		5.059343495E-9, -9.91364156E-10, -2.27365122E-10, 9.6467911E-11,
+		2.394038E-12, -6.886027E-12, 8.94487E-13, 3.13092E-13, -1.12708E-13,
+		3.81E-16, 7.106E-15, -1.523E-15, -9.4E-17, 1.21E-16, -2.8E-17];
+	var ncof = coefficients.length;
+	var d = 0.0;
+	var dd = 0.0;
+	for (var j = ncof - 1; j > 0; j--) {
+		var tmp = d;
+		d = ty * d - dd + coefficients[j];
+		dd = tmp;
+	}
+	var ans = t * Math.exp(-z * z + 0.5 * (coefficients[0] + ty * d) - dd);
+	return x >= 0.0 ? ans : (2.0 - ans);
+};
+
+/**
+ * Return the value of the specified gaussian at the specified value.
+ *
+ * N.B. If mean and/or standard deviation are not specified the
+ * normalized values will be used.
+ *
+ * <pre>
+ *				1			-(x-μ)^2 / (2*σ^2)
+ * P(x)=--------------- * e
+ *		σ * sqrt(2*pi)
+ * </pre>
+ *
+ * @param x the location to evaluate the specified gaussian at
+ * @return the value at x of the specified gaussian
+ * @see http://mathworld.wolfram.com/NormalDistribution.html
+ */
+GaussianDistribution.at = function(x, mean, standardDeviation) {
+	// Check arguments
+	if (!mean) {
+		mean = 0;
+	}
+	if (!standardDeviation) {
+		standardDeviation = 1;
+	}
+
+	var multiplier = 1.0 / (standardDeviation * Math.sqrt(2 * Math.PI));
+	var expPart = Math.exp((-1.0 * Math.pow(x - mean, 2.0)) / (2 * (standardDeviation * standardDeviation)));
+	var result = multiplier * expPart;
+	return result;
+};
 
 /**
  * The Gaussian representation of a flat line.
--- test/server/racingjellyfish/jstrueskill/numerics/GaussianDistributionTests.js
+++ test/server/racingjellyfish/jstrueskill/numerics/GaussianDistributionTests.js
@@ -15,3 +15,24 @@ exports.testBasicValues = function(test) {
 
 	test.done();
 };
+
+exports.testCumulativeTo = function(test) {
+	// Verified with WolframAlpha
+	// (e.g. http://www.wolframalpha.com/input/?i=CDF%5BNormalDistribution%5B0%2C1%5D%2C+0.5%5D )
+	var expected = 0.691462461;
+	testExt.equalsWithTolerance(test, GaussianDistribution.cumulativeTo(0.5), expected,
+		ERROR_TOLERANCE, "Expected gaussian 'cumulativeTo' value to be " + expected);
+
+	test.done();
+};
+
+exports.testAt = function(test) {
+	// Verified with WolframAlpha
+	// (e.g. http://www.wolframalpha.com/input/?i=PDF%5BNormalDistribution%5B0%2C1%5D%2C+0.5%5D )
+	var expected = 0.352065326;
+	testExt.equalsWithTolerance(test, GaussianDistribution.at(0.5), expected,
+		ERROR_TOLERANCE, "Expected gaussian 'at' value to be " + expected);
+
+	test.done();
+};
+
