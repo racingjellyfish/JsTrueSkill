cf7f14e40b00976a7402ac3d9c78522ce90e8da3
Adrian Conlin <adrian.conlin@gmail.com>
2013-07-27 22:08:20 +0200

    Implemented a gaussian factor to sum together multiple gaussians and one to represent a team difference that has not exceeded the draw margin.

---
+++ src/racingjellyfish/jstrueskill/trueskill/factors/GaussianWithinFactor.js
@@ -0,0 +1,66 @@
+var MathUtils = require('../numerics/MathUtils');
+var util = require('util');
+var GaussianDistribution = require('../../numerics/GaussianDistribution');
+var GaussianFactor = require('./GaussianFactor');
+var Message = require('../../factorgraphs/Message');
+var Variable = require('../../factorgraphs/Variable');
+var TGCF = require('../TruncatedGaussianCorrectionFunctions');
+
+/**
+ * Factor representing a team difference that has not exceeded the draw margin.
+ * <remarks>See the accompanying math paper for more details.</remarks>
+ */
+var GaussianWithinFactor = function(epsilon, variable) {
+	GaussianFactor.call(this, util.format('%s <= %4.3f', variable, epsilon));
+
+	this.epsilon = epsilon;
+	this.createVariableToMessageBinding(variable);
+};
+
+GaussianWithinFactor.prototype = new GaussianFactor();
+
+GaussianWithinFactor.prototype.getLogNormalization = function() {
+	var marginal = variables[0].getValue();
+	var message = messages[0].getValue();
+	var messageFromVariable = divide(marginal, message);
+	var mean = messageFromVariable.getMean();
+	var std = messageFromVariable.getStandardDeviation();
+	var z = GaussianDistribution.cumulativeTo((this.epsilon - mean) / std) -
+		GaussianDistribution.cumulativeTo((-this.epsilon - mean) / std);
+
+	return -GaussianDistribution.logProductNormalization(messageFromVariable, message) +
+		Math.log(z);
+};
+
+GaussianWithinFactor.prototype.updateMessage = function(message, variable) {
+	var oldMarginal = new GaussianDistribution(variable.getValue());
+	var oldMessage = new GaussianDistribution(message.getValue());
+	var messageFromVariable = GaussianDistribution.divide(oldMarginal,oldMessage);
+
+	var c = messageFromVariable.getPrecision();
+	var d = messageFromVariable.getPrecisionMean();
+
+	var sqrtC = Math.sqrt(c);
+	var dOnSqrtC = d / sqrtC;
+
+	var epsilonTimesSqrtC = this.epsilon * sqrtC;
+	d = messageFromVariable.getPrecisionMean();
+
+	var denominator = 1.0 - TGCF.wWithinMargin(dOnSqrtC, epsilonTimesSqrtC);
+	var newPrecision = c / denominator;
+	var newPrecisionMean = (d + sqrtC * TGCF.vWithinMargin(dOnSqrtC, epsilonTimesSqrtC)) /
+		denominator;
+
+	var newMarginal = GaussianDistribution.fromPrecisionMean(newPrecisionMean, newPrecision);
+	var newMessage = GaussianDistribution.divide(
+		GaussianDistribution.multiply(oldMessage,newMarginal), oldMarginal);
+
+	// Update the message and marginal
+	message.setValue(newMessage);
+	variable.setValue(newMarginal);
+
+	// Return the difference in the new marginal
+	return GaussianDistribution.sub(newMarginal, oldMarginal);
+};
+
+module.exports = GaussianWithinFactor;
