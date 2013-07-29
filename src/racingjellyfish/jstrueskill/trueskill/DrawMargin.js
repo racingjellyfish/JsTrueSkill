d9597f3073f1fca9196b2b5e980df92384e67c92
Adrian Conlin <adrian.conlin@gmail.com>
2013-07-29 10:05:51 +0200

    Implemented calculation of draw margin from draw probability.

---
+++ src/racingjellyfish/jstrueskill/trueskill/DrawMargin.js
@@ -0,0 +1,16 @@
+var GaussianDistribution = require('../numerics/GaussianDistribution');
+
+exports.getDrawMarginFromDrawProbability = function(drawProbability, beta) {
+	// Derived from TrueSkill technical report (MSR-TR-2006-80), page 6
+
+	// draw probability = 2 * CDF(margin/(sqrt(n1+n2)*beta)) -1
+
+	// implies
+	//
+	// margin = inversecdf((draw probability + 1)/2) * sqrt(n1+n2) * beta
+	// n1 and n2 are the number of players on each team
+	var margin = GaussianDistribution.inverseCumulativeTo(0.5 *
+		(drawProbability + 1), 0, 1) * Math.sqrt(1 + 1) * beta;
+
+	return margin;
+};
