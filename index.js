c3d4fb68be5226e18d61bc94970c88cdcd40691d
Adrian Conlin <adrian.conlin@gmail.com>
2013-08-21 21:20:50 +0100

    Added key classes to the module API.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-module.exports = require('./lib/racingjellyfish/jstrueskill/TrueSkillCalculator');
+exports.GameInfo = require('./lib/racingjellyfish/jstrueskill/GameInfo');
+exports.Player = require('./lib/racingjellyfish/jstrueskill/Player');
+exports.Rating = require('./lib/racingjellyfish/jstrueskill/Rating');
+exports.Team = require('./lib/racingjellyfish/jstrueskill/Team');
+exports.FactorGraphTrueSkillCalculator = require('./lib/racingjellyfish/jstrueskill/trueskill/FactorGraphTrueSkillCalculator');
